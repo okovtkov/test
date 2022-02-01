def95176b8477c9ea35bb77f01f2447e0ada0aa7
vasilich99 <oleg_kovtkov@mail.ru>
2022-02-01 17:27:58 +0300

    feat: create a button component

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -5,18 +5,18 @@ export default function Card() {
   return (
     <div className="card">
       <div className="card__item">
-        <span className='card__key'>ФИО: </span>
-        <span className='card__value'>Иван Иванов</span>
+        <span className="card__key">ФИО: </span>
+        <span className="card__value">Иван Иванов</span>
       </div>
       <div className="card__item">
-        <span className='card__key'>город: </span>
-        <span className='card__value'>Москва</span>
+        <span className="card__key">город: </span>
+        <span className="card__value">Москва</span>
       </div>
       <div className="card__item">
-        <span className='card__key'>компания: </span>
-        <span className='card__value'>ООО "Пример"</span>
+        <span className="card__key">компания: </span>
+        <span className="card__value">ООО "Пример"</span>
       </div>
-      <Link className="card__more" to='/profile'>Подробнее</Link>
+      <Link className="card__more" to="/profile">Подробнее</Link>
     </div>
   )
 }
--- src/components/sort/sort.tsx
+++ src/components/sort/sort.tsx
@@ -1,11 +1,12 @@
+import Button from '../button/button';
 import './sort.scss';
 
 export default function Sort() {
   return (
     <div className="sort">
       <span className="sort__label">Сортировка</span>
-      <button className="sort__button sort__by-city">по городу</button>
-      <button className="sort__button sort__by-company">по компании</button>
+      <Button name="по городу" className="sort__by-city" />
+      <Button name="по компании" className="sort__by-company" />
     </div>
   )
 }
